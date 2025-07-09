ff2c0dbefefdd1825421a526163ecb83fe6442b0
nirajankarki5 <nirajankarki5@gmail.com>
2025-07-09 17:58:18 -0400

    added resource path and default-page-size

--- src/main/java/com/example/restdb/dao/EmployeeRepository.java
+++ src/main/java/com/example/restdb/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.restdb.dao;
 
 import com.example.restdb.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // that's it.. no coded needed
 }
